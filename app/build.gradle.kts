490f9c495bab72f6880b8b909a73a31a7bfe5ba8
dlwns <dlwns@example.com>
2026-07-09 00:50:25 +0900

    chore: 앱 버전을 2.0.2로 올림

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.carrotnavi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 43
-        versionName = "2.0.1"
+        versionCode = 44
+        versionName = "2.0.2"
     }
 
     buildTypes {
